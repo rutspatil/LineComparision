5d3d9ee9328646ecfe57bff91f882e08edf1ea53
rutuja patil <rutujapatil991@gmail.com>
2022-03-06 20:20:42 +0530

    Check The Lines Equility

--- LineCoparision/Program.cs
+++ LineCoparision/Program.cs
@@ -10,8 +10,8 @@ namespace LineCoparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to line comparision computation program");
             //varibles declaration
+            //varibles
             double x1, x2, y1, y2;
             double len, length;
             double point1, point2;
@@ -20,11 +20,38 @@ namespace LineCoparision
             Console.WriteLine("Enter value of x1 : ");
             x1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter value of y1 : ");
+            double x11, x22, y11, y22;
+            double len1, length1;
+            double point3, point4;
+
+            //taking user input of line 1
+            Console.WriteLine("Enter the coordinates for line number 1");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter x1 of line1");
+            x1 = Convert.ToDouble(Console.ReadLine()); //Converting string to double
+            Console.WriteLine("Enter y1 of line1");
             y1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter value of x2 : ");
+            Console.WriteLine("Enter x2 of line1");
             x2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter value of y2 : ");
+            Console.WriteLine("Enter y2 of line1");
             y2 = Convert.ToDouble(Console.ReadLine());
+
+            //taking user inputs of line 2
+            Console.WriteLine();
+            Console.WriteLine("Enter the coordinates for line number 2");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter x1 of line2");
+            x11 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y1 of line2");
+            y11 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x2 of line2");
+            x22 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y2 of line2");
+            y22 = Convert.ToDouble(Console.ReadLine());
 
             //using the math class methods pow and sqrt for calculation
             // formula of length = sqrt[(x2 -x1)^2 + (y2-y1)^2]
@@ -34,9 +61,26 @@ namespace LineCoparision
             length = Math.Sqrt(len);
 
             Console.WriteLine("The length of the line : " + length);
+            point3 = Math.Pow((x22 - x11), 2);
+            point4 = Math.Pow((y22 - y11), 2);
+            len1 = point3 + point4;
+            length1 = Math.Sqrt(len1);
+
+            Console.WriteLine("The length of line 1 : " + length);
+            Console.WriteLine("The length of line 2 : " + length1);
+            Console.WriteLine();
+
+            if (length == length1)
+            {
+                Console.WriteLine("Both the lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("The lines aren't equal");
+            }
+
+
 
         }
-
-
     }
 }
